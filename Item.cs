e755ae7661ed9463301875aa74f27b8ffb777791
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2019-07-16 20:05:14 +0200

    Add files via upload

---
+++ Item.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public Collider colider;
+
+    void Start()
+    {
+        colider = GetComponent<Collider>();
+    }
+        void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+            {
+            colider.isTrigger = true;
+        }
+        else
+        {
+            colider.isTrigger = false;
+        }
+
+    }
+    
+
+  void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag== "Player" ) 
+        {
+            Destroy(gameObject);
+        }
+
+    }
+     
+    }
+ 
+
