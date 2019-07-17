57fb07756e0fdb14140c912d4d75a603196010a5
muerteloca12 <43545742+muerteloca12@users.noreply.github.com>
2019-07-17 20:10:38 +0200

    Update Item.cs

--- Item.cs
+++ Item.cs
@@ -1,30 +1,54 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Item : MonoBehaviour
 {
     public Collider colider;
+    public string mensaje="Pulsa E para cogerlo";
+    public bool activo=false;
+    public GUIStyle font;
+    
+    
+
 
     void Start()
     {
+        
         colider = GetComponent<Collider>();
+        
+
     }
-        void Update ()
+    void Update ()
     {
         if(Input.GetKeyDown(KeyCode.E))
             {
             colider.isTrigger = true;
+            
         }
         else
         {
             colider.isTrigger = false;
+           
         }
 
     }
-    
+    void OnGUI()
+    {
 
-  void OnTriggerEnter(Collider col)
+        if (activo)
+        {
+
+           
+            GUI.Label(new Rect(Screen.width / 2 - 100, 50, 200, 30), mensaje, font );
+          
+
+        }
+
+    }
+
+        void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag== "Player" ) 
         {
@@ -32,7 +56,25 @@ public class Item : MonoBehaviour
         }
 
     }
-     
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            activo = true;
+        }
+        
+            
+        
     }
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            activo = false;
+        }
+    }
+    
+    }
+
  
 
